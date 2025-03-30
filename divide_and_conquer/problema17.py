7110f3dcfa128f57139d312850271ecfc0abf8b1
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-03-30 22:34:29 +0200

    Solved exercise 17 of D&C

---
+++ divide_and_conquer/problema17.py
@@ -0,0 +1,37 @@
+from typing import List
+
+
+# Complessita O(log n)
+# T(n) = T(n) + b
+# a = 1, c = 2, k = 0
+# a = c^k
+def find_el_helper(array: List[List[int]], k: int, row: int, col: int):
+    if row >= len(array) or col < 0:
+        return None
+    if array[row][col] == k:
+        return (row, col)
+    if array[row][col] > k:
+        return find_el_helper(array, k, row + 1, col)
+    return find_el_helper(array, k, row, col - 1)
+    
+
+
+def find_el(array: List[List[int]], k: int):
+    return find_el_helper(array, k, 0, len(array[0]) - 1)
+
+
+
+    
+
+def main():
+    array = [
+        [9, 8, 7],
+        [6, 5, 4],
+        [3, 2, 1],
+    ]
+    sol = find_el(array, int(input("Inserisci numero: ")))
+    print(sol)
+    return
+
+if __name__ == "__main__":
+    main()
