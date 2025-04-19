5dec8536e968a53e1381413c3deda8fc28b9c599
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-04-19 22:37:40 +0200

    Solved exercise number 9 of DP

---
+++ dynamic_programming/problema09.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+x = 4
+y = 2
+z = 5
+
+
+# Time complexity
+# O(n^2)
+def solve(n: int, p: List[int], memo: List[int]):
+    if n == 0:
+        return 0
+    if memo[n] is None:
+        memo[n] = max(solve(n - i, p, memo) + el for i, el in enumerate(p[:n], 1))
+
+    return memo[n]
+
+
+def main():
+    a = list(map(lambda x: int(x), input("Inserisci pesi: ").split(" ")))
+    n = len(a)
+    memo = [None for i in range(n + 1)]
+    print(solve(n, a, memo))
+    return
+
+
+if __name__ == "__main__":
+    main()
