dfd3edf0512a389429b2d020e2dc65d5ce62dddf
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-04-19 22:54:54 +0200

    Fixed another error in dp problem 10

--- dynamic_programming/problema10.py
+++ dynamic_programming/problema10.py
@@ -8,18 +8,18 @@ z = 5
 
 # Time complexity
 # O(Cn)
-def solve(crediti: int, d: List[int], c: List[int], e_ind: int, memo: List[int]):
+def solve(crediti: int, d: List[int], c: List[int], e_ind: int, memo: List[[int]]):
     if crediti <= 0:
         return 0
     if e_ind < 0:
         return float("-inf")
-    if memo[crediti] is None:
-        memo[crediti] = min(
+    if memo[crediti][e_ind] is None:
+        memo[crediti][e_ind] = min(
             solve(crediti, d, c, e_ind - 1, memo),
             solve(crediti - c[e_ind], d, c, e_ind - 1, memo) + d[e_ind],
         )
 
-    return memo[crediti]
+    return memo[crediti][e_ind]
 
 
 def main():
@@ -27,7 +27,7 @@ def main():
     crediti = list(map(lambda x: int(x), input("Inserisci crediti: ").split(" ")))
     crediti_totali = int(input("Inserisci crediti totali: "))
     n = len(crediti)
-    memo = [None for i in range(n + 1)]
+    memo = [[None for i in range(n + 1)] for _ in range(crediti_totali + 1)]
     print(solve(crediti_totali, difficolta, crediti, n - 1, memo))
     return
 
