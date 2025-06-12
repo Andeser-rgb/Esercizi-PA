73449d421bc441d2f204e9625a21b18c47dfa0ab
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-12 15:51:02 +0200

    Solved problem 19 of graph problems

---
+++ graphs/problema19.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+def is_vertex_cover(graph: list[list[int]], s: list[int]):
+    contains = [False] * len(graph)
+    for el in s:
+        contains[el] = True
+
+    for i in range(len(graph)):
+        for j in range(i + 1, len(graph)):
+            if not contains[i] and not contains[j]:
+                return False
+    return True
+
+
+def is_vertex_cover_adj(graph: list[list[int]], s: list[int]):
+    contains = [False] * len(graph)
+    for el in s:
+        contains[el] = True
+
+    for i, adj_l in enumerate(graph):
+        for j in adj_l:
+            if not contains[i] and not contains[j]:
+                return False
+    return True
+       
