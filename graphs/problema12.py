3136fccf8f35fbea37d82e87b1d20063541d42f9
Andrea Squitieri <andrea.squitieri@disroot.org>
2025-06-12 09:57:58 +0200

    Solved problem 12 of graph problems

---
+++ graphs/problema12.py
@@ -0,0 +1,9 @@
+def squared_graph(graph: list[list[int]]):
+    s_graph = [[0] * len(graph)] * len(graph)
+    for i in range(len(graph)):
+        for j in range(len(graph)):
+            if graph[i][j] == 1:
+                for k in range(len(graph)):
+                    s_graph[i][k] |= 1
+        
+    return s_graph
